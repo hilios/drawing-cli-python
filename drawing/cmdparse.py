1cb6e97a5bac028cbfef7fba3d173d7eec9ea993
Edson Hilios <edson.hilios@gmail.com>
2017-04-10 01:26:37 -0300

    Improve base cmd parser and formatter

--- drawing/cmdparse.py
+++ drawing/cmdparse.py
@@ -4,40 +4,43 @@ import logging
 import re
 
 
-CMD_PARSER = re.compile("^(%s)$" % "|".join("""
-(C) (\d+) (\d+)
-(L) (\d+) (\d+) (\d+) (\d+)
-(R) (\d+) (\d+) (\d+) (\d+)
-(B) (\d+) (\d+) (\w)
-(Q)
-""".split("\n")))
+CMD_PARSER = re.compile("[A-Z]{1}(\s(\d+|[a-z]{1}))*$")
 
 
-def action(action, require_canvas=True):
-    "Defines an action constraint."
+def parse(cmd):
+    "Parses a command input"
+    if CMD_PARSER.match(cmd):
+        return tuple(cmd.split())
+    else:
+        logging.warn("Invalid input")
+        return (None,)
+
+
+def action(expected_action, fmts=tuple(), require_canvas=True):
+    "Defines an action constraint and format"
     def action_decorator(fn):
         def wrapper(cmd, drawing):
-            if cmd[0] is not action:
+            action, params = (cmd[0], cmd[1:])
+
+            if action is not expected_action:
                 return drawing
 
             if require_canvas and drawing is None:
                 logging.warn('Please, create a canvas before drawing')
                 return drawing
 
-            logging.info("Executing command: %s" % " ".join(cmd))
-            return fn(cmd, drawing)
+            try:
+                if len(params) is not len(fmts):
+                    raise ValueError
+
+                fmtp = tuple([fmt(param) for (param, fmt) in zip(params, fmts)])
+                logging.debug("Executing %s with params: %s" % (action, fmtp))
+                return fn(fmtp, drawing)
+
+            except ValueError:
+                logging.warn("Wrong input")
+                return drawing
 
         return wrapper
 
     return action_decorator
-
-
-def parse(cmd):
-    "Given an cmd input "
-    parser = CMD_PARSER.match(cmd)
-    if parser:
-        cmd_group = parser.groups()
-        return tuple([val for val in cmd_group[1:] if val != None])
-    else:
-        logging.warn("Invalid input")
-        return (None,)
--- drawing/cmdparse_test.py
+++ drawing/cmdparse_test.py
@@ -1,10 +1,52 @@
 import unittest
+from drawing.cmdparse import action, parse
 
 
-class CmdparseTest(unittest.TestCase):
+@action('A', fmts=(int, str,), require_canvas=True)
+def test_cmd(cmd, drawing):
+    return cmd + (drawing,)
 
-  def setUp(self):
-    pass
 
-  def test_parse(self):
-    self.assertEqual(1, 1)
+@action('B', require_canvas=False)
+def test_cmd_wo_canvas(cmd, drawing):
+    return cmd + (drawing,)
+
+
+class ActionTest(unittest.TestCase):
+
+    def test_action_w_fmt_and_require_canvas(self):
+        "If canvas provided return the function or returns the drawing otherwise"
+        self.assertEqual(test_cmd(('A', '1', 'c'), "  "), (1, 'c', "  "))
+        self.assertEqual(test_cmd(('A', '1', 'c'), None), None)
+
+    def test_action_wo_fmt_or_require_canvas(self):
+        "Always return the function no matter what"
+        self.assertEqual(test_cmd_wo_canvas(('B',), "  "), ("  ",))
+        self.assertEqual(test_cmd_wo_canvas(('B',), None), (None,))
+
+    def test_fmt_incorrect_args(self):
+        "If params not match the formatters return the drawing"
+        self.assertEqual(test_cmd(('A', '1', 'c', '2'), "  "), "  ")
+        self.assertEqual(test_cmd(('A', '1',), "  "), "  ")
+
+    def test_fmt_value_error(self):
+        "If fmt could not be applied return the drawing"
+        self.assertEqual(test_cmd(('A', 'c', '1'), "  "), "  ")
+        self.assertEqual(test_cmd(('A', 'c', '1'), None), None)
+
+
+
+class ParseTest(unittest.TestCase):
+
+    def test_correct_matches(self):
+        self.assertEqual(parse("C 20 4"), ("C", "20", "4",))
+        self.assertEqual(parse("L 1 2 3 4"), ("L", "1", "2", "3", "4",))
+        self.assertEqual(parse("R 1 2 3 4"), ("R", "1", "2", "3", "4",))
+        self.assertEqual(parse("B 1 2 c"), ("B", "1", "2", "c"))
+        self.assertEqual(parse("Q"), ("Q",))
+
+    def test_incorrect_matches(self):
+        self.assertEqual(parse("B 1 ccc"), (None,))
+        self.assertEqual(parse("CC 20 30"), (None,))
+        self.assertEqual(parse("q"), (None,))
+        self.assertEqual(parse("Lorem ipsum dolor"), (None,))
