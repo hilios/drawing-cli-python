d16be2bdd6938645aba336bb54436c094e7feb17
Edson Hilios <edson.hilios@gmail.com>
2017-04-10 04:27:43 -0300

    Add a basic renderer

--- drawing/__init__.py
+++ drawing/__init__.py
@@ -1,6 +1,7 @@
 """Main drawing functions
 """
 import sys
+import re
 from drawing import cmdparse, draw
 
 
@@ -11,7 +12,7 @@ def q(drawing):
 
 @cmdparse.handle('C', int, int, require_canvas=False)
 def c(w, h, drawing):
-    return draw.canvas(w, h, drawing)
+    return "%s\t%d\t%d" % (draw.canvas(w, h, drawing), w, h)
 
 
 @cmdparse.handle('L', int, int, int, int)
@@ -29,7 +30,22 @@ def b(x1, y1, c, drawing):
     return draw.fill(x1, y1, c, drawing)
 
 
+def lines(width, data, lines=[]):
+    head, tail = data[0:width], data[width:]
+    if tail:
+        lines(width, tail, [head] + lines)
+
+
 def render(cmd, drawing):
     drawing = reduce(lambda x, fn: fn(cmd, x), [q, c, l, r, b], drawing)
+
+    if drawing:
+        d, w, h = drawing.split("\t")
+        width, height = int(w), int(h)
+        print "\n".join(
+            ["+{header}+"] +
+            ["|%s|" % d[(i * width):(i * width + width)] for i in xrange(height)] +
+            ["+{footer}+"]
+        ).format(header="-" * width, footer="-" * width)
 
     return drawing
